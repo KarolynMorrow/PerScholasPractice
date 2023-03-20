d60636e94b125a55bf129e5a41bf578582bb281e
Karolyn Morrow <karolyn.morrow@gmail.com>
2023-03-20 17:25:57 -0400

    This is the second commit

--- src/main/java/gitHomework.java
+++ src/main/java/gitHomework.java
@@ -3,6 +3,9 @@ public class gitHomework {
     public static void main(String[] args) {
 
         System.out.println("Hello world, This is practice.");
+    }
 
+    public static void secondCommit(){
+        System.out.println("This is considered the second commit.");
     }
 }

283a537fec3ef6e76f66e2ad132d9f1929a1bdbe
Karolyn Morrow <karolyn.morrow@gmail.com>
2023-03-20 17:28:18 -0400

    This is the third commit

--- src/main/java/gitHomework.java
+++ src/main/java/gitHomework.java
@@ -2,10 +2,17 @@ public class gitHomework {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello world, This is practice.");
+        System.out.println("Hello world, This is practice for commit. This is the first commit updated during the third commit");
+        secondCommit();
+        thirdCommit();
+
     }
 
     public static void secondCommit(){
         System.out.println("This is considered the second commit.");
+    }
+
+    public static void thirdCommit(){
+        System.out.println("This is the third and final commit.");
     }
 }
